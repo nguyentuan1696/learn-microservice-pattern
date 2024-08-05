25e77f645f0542eb56408b8151126c3609781035
nguyentuan1696 <nguyentuan1696@gmail.com>
2024-08-05 14:45:43 +0700

    setup http

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Note learn microservice pattern
+# Learn microservice pattern
 
+
+## Note
+
+## Links
+
+- [Saga distributed transactions pattern](https://learn.microsoft.com/en-us/azure/architecture/reference-architectures/saga/saga)
--- order-service/main.go
+++ order-service/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	type OrderRes struct {
+		OrderId int `json:"id"`
+	}
+
+	e.POST("/orders", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, &OrderRes{
+			OrderId: 123,
+		})
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
